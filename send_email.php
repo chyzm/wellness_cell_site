8fafa2741bdf600d6d0888b2eaeaa36051cf2a24
chyzm <somtochiokaro@gmail.com>
2025-03-05 14:53:29 +0100

    test

--- send_email.php
+++ send_email.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "healthritenig@gmail.com";
+    $to = "suavedef@gmail.com";
     $userEmail = $_POST['email'];
     $subject = "Daily Wellness Report Submission";
     
