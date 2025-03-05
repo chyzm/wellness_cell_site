16e4f8b41e1d18dbd5f58eecaa8af7666eb23b05
chyzm <somtochiokaro@gmail.com>
2025-03-05 16:08:29 +0100

    whatsapp server

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+function sendToWhatsApp(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const form = document.getElementById('wellnessForm');
+    const formData = new FormData(form);
+
+    let message = "Wellness Cell Site Daily Report:\n\n";
+
+    formData.forEach((value, key) => {
+        message += `${key}: ${value}\n`;
+    });
+
+    const whatsappNumber = "2347031016787"; // Your WhatsApp number
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappURL, '_blank'); // Open WhatsApp in a new tab
+}
+
+function toggleContactOptions() {
+    const contactOptions = document.getElementById('contactOptions');
+    contactOptions.classList.toggle('active');
+}
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Add smooth scrolling behavior
+    });
+}
+
+
 // Back to Top Functionality
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
